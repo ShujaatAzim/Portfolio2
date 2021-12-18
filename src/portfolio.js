dee5900155e89250e5550d5c665d405641631ad7
Shujaat Azim <shujaat.azim@gmail.com>
2021-12-17 15:38:47 -1000

    Started editing template. First commit.

--- src/portfolio.js
+++ src/portfolio.js
@@ -1,15 +1,15 @@
 const header = {
   // all the properties are optional - can be left empty or deleted
-  homepage: 'https://rajshekhar26.github.io/cleanfolio',
-  title: 'JS.',
+  homepage: 'https://shujaatazim.github.io/',
+  title: 'SA',
 }
 
 const about = {
   // all the properties are optional - can be left empty or deleted
-  name: 'John Smith',
-  role: 'Front End Engineer',
+  name: 'Shujaat Azim',
+  role: 'Full Stack Software Developer',
   description:
-    'Adipisicing sit fugit ullam unde aliquid sequi Facilis soluta facilis perspiciatis corporis nulla aspernatur. Autem eligendi rerum delectus modi quisquam? Illo ut quasi nemo ipsa cumque perspiciatis! Maiores minima consectetur.',
+    'I am an enthusiastic full stack software engineer with a background in, and passion for technology, medicine, and finance. I am a seasoned professional with a diverse background in many fields, which enables me to quickly adopt and master new technologies while working in both team and self-directed settings. I am always eager to continue learning and improving!',
   resume: 'https://example.com',
   social: {
     linkedin: 'https://linkedin.com',
@@ -65,7 +65,7 @@ const skills = [
 
 const contact = {
   // email is optional - if left empty Contact section won't show up
-  email: 'johnsmith@mail.com',
+  email: 'shujaat.azim@gmail.com',
 }
 
 export { header, about, projects, skills, contact }
